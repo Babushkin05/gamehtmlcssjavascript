b1490f963b92863ebca3be31d5ad9d8833ded69c
Babushkin05 <91776460+Babushkin05@users.noreply.github.com>
2021-12-20 14:05:22 +0700

    Update script.js

--- game html/script.js
+++ game html/script.js
@@ -1,23 +1,96 @@
 const gameboard = document.getElementById("gameboard")
 const platform = document.getElementById("platform")
 const ball = document.getElementById("ball")
-
+var res =true
 var platfObj = new(Object) 
 // var ballInTheGameboard;
+console.log(KeyboardEvent)
 
+var gameLevel = [
+    { x: 20, y: 100 },
+    { x: 75, y: 100 },
+    { x: 130, y: 100 },
+    { x: 185, y: 100 },
+    { x: 240, y: 100 },
+    { x: 295, y: 100 },
+    { x: 350, y: 100 },
+    { x: 405, y: 100 },
+    { x: 460, y: 100 },
+    { x: 515, y: 100 },
+    { x: 570, y: 100 },
+    { x: 625, y: 100 },
+    // { x: 680, y: 100 },
 
+    { x: 20, y: 130 },
+    { x: 75, y: 130 },
+    { x: 130, y: 130 },
+    { x: 185, y: 130 },
+    { x: 240, y: 130 },
+    { x: 295, y: 130 },
+    { x: 350, y: 130 },
+    { x: 405, y: 130 },
+    { x: 460, y: 130 },
+    { x: 515, y: 130 },
+    { x: 570, y: 130 },
+    { x: 625, y: 130 },
 
+    { x: 20, y: 160 },
+    { x: 75, y: 160 },
+    { x: 130, y: 160 },
+    { x: 185, y: 160 },
+    { x: 240, y: 160 },
+    { x: 295, y: 160 },
+    { x: 350, y: 160 },
+    { x: 405, y: 160 },
+    { x: 460, y: 160 },
+    { x: 515, y: 160 },
+    { x: 570, y: 160 },
+    { x: 625, y: 160 },
 
+    { x: 20, y: 190 },
+    { x: 75, y: 190 },
+    { x: 130, y: 190 },
+    { x: 185, y: 190 },
+    { x: 240, y: 190 },
+    { x: 295, y: 190 },
+    { x: 350, y: 190 },
+    { x: 405, y: 190 },
+    { x: 460, y: 190 },
+    { x: 515, y: 190 },
+    { x: 570, y: 190 },
+    { x: 625, y: 190 },
+
+    { x: 20, y: 220 },
+    { x: 75, y: 220 },
+    { x: 130, y: 220 },
+    { x: 185, y: 220 },
+    { x: 240, y: 220 },
+    { x: 295, y: 220 },
+    { x: 350, y: 220 },
+    { x: 405, y: 220 },
+    { x: 460, y: 220 },
+    { x: 515, y: 220 },
+    { x: 570, y: 220 },
+    { x: 625, y: 220 },
+
+    
+   
+    
+ ]
+ 
+ gameLevel.forEach(createBrick)
+
+if(res==true){
 platfObj.X = platform.offsetLeft
 platfObj.Y = platform.offsetTop
 platfObj.L = platform.clientWidth
-platfObj.dX = 10
+platfObj.dX = 10}
 
 var ballObj = new(Object)
-ballObj.X = ball.offsetLeft 
-ballObj.Y = ball.offsetTop
+ballObj.X = 300
+ballObj.Y = 400
 ballObj.dX = 2.5
-ballObj.dY = 2.5
+ballObj.dY = -2.5
 
 // if(ballObj.X>=0 && ballObj.X<=gameboard.offsetWidth && ballObj.Y>=0 && ballObj<=gameboard.offsetHeight) {
 //     ballInTheGameboard = true;
@@ -58,5 +131,38 @@ function moveBall() {
         ball.style.top = ballObj.Y + "px"
         window.requestAnimationFrame(moveBall)
 }
+else{
+    res=confirm("ты проиграл(( хочешь еще раз?")
+    if (res==true) {
+        ballObj.X = 300
+        ballObj.Y = 400
+        ballObj.dY = -2.5
+        ballObj.dX = 2.5
+        ball.style.left = ballObj.X + "px"
+        ball.style.top = ballObj.Y + "px"
+        window.requestAnimationFrame(moveBall)
+
+        
+    }
+    if(res==true){
+        platfObj.X = platform.offsetLeft
+        platfObj.Y = platform.offsetTop
+        platfObj.L = platform.clientWidth
+        platfObj.dX = 10}
+    
 }
+}
+
+
+
 window.requestAnimationFrame(moveBall)
+
+
+function createBrick(brick) {
+    var el = document.createElement("div")
+    el.classList.add("brick")
+    el.style.top = brick.y + "px"
+    el.style.left = brick.x + "px"
+ 
+    gameboard.appendChild(el)
+ }
